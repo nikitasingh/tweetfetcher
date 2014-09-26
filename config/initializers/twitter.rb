9225502d4d3c3c37df6779b690df92e1073b9291
Nikita singh <nikitaa_singh@yahoo.co.in>
2014-09-26 12:28:08 +0000

    intial commit

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter::REST::Client.new do |config|
+  config.consumer_key        = "0EjV9pC0SzaNThnUUneikFnq6"
+  config.consumer_secret     = "2YVAGVJGs5ikOXeMnyVIjF35AmjC4E2cdOEBtkkGM2DebN6fzp"
+  config.access_token        = "616382835-WbT02Ttvcmn24GTvgXbEcSFV5shSWFc5eHyCmCqZ"
+  config.access_token_secret = "Pw0os2QQFtRfr0JbYhG7MODwpKEMtrGvzWzbFjdQV2pKK"
+end
